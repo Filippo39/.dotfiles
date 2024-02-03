aa3c456f9830535ca2f497645d9bc0a0bd693820
Filippo Mantovan <filippomantovan39@gmail.com>
2024-02-03 11:44:00 +0100

    add dotfiles

---
+++ .config/dunst/notification_alert.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+paplay ~/.config/dunst/sci_fi_notification.wav
---
+++ .config/polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+#polybar top -c ~/.config/polybar/config.ini &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo $m
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload example &
+fi
